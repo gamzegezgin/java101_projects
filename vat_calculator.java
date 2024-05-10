cb4687ecf736158c0d7c8281b0d1cbca5dd59407
Gamze Gezgin <gamzegezgin@yahoo.com>
2024-05-11 02:55:21 +0300

    created vat calculator

--- vat_calculator.java
+++ vat_calculator.java
@@ -1,9 +1,28 @@
 package java101Projects;
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class vat_calculator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		double price;
+		
+		Scanner input= new Scanner(System.in);
+		
+		System.out.println("Please enter the price: ");
+		price = input.nextDouble();
+		
+		double priceWithVat = price + price*0.18;
+		
+		double vatAmount = price*0.18;
+		
+        DecimalFormat df = new DecimalFormat("#.##");
+        String formattedPriceWithVat = df.format(priceWithVat);
+        String formattedVatAmount = df.format(vatAmount);
+
+        System.out.println("Value Added Tax amount: " + formattedPriceWithVat + "\n" + "Amount of Tax: " + formattedVatAmount);
+		
 
 	}
 
