8fbdb62be7392f4d7d4dfbd0a1a85ac84f88a5fc
Gamze Gezgin <gamzegezgin@yahoo.com>
2024-05-17 23:01:00 +0300

    scsanner class had been added

--- horoscope.java
+++ horoscope.java
@@ -1,10 +1,14 @@
 package java101Projects;
 
+import java.util.Scanner;
+
 public class horoscope {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		System.out.println("write number of month which you born: ");
+		int  month = input.nextInt();
 	}
 
 }
